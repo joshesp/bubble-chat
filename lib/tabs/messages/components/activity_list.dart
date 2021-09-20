dc05ac3e0b2508161c89b3dbe8d2851aa43774ac
Josué <joshes@me.com>
2021-09-19 22:29:00 -0500

    componente para el listado de la actividad en la vista de mensajes

---
+++ lib/tabs/messages/components/activity_list.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../../components/user_avatar/user_avatar.dart';
+import '../../../constants.dart';
+import '../../../dummy_data/messages_data.dart';
+import '../../../helpers/strings_helper.dart';
+
+class ActivityListComponent extends StatelessWidget {
+  const ActivityListComponent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return InkWell(
+              child: Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: defaultSpacing / 2),
+                child: Column(
+                  children: [
+                    UserAvatarComponent(
+                      imageUrl: lastActivityList[index]['image'],
+                      radius: 28,
+                      showBorder: true,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      '@' +
+                          truncateWithEllipsis(
+                              5, lastActivityList[index]['username']),
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                  ],
+                ),
+              ),
+              onTap: () {});
+        },
+        itemCount: lastActivityList.length,
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+      ),
+    );
+  }
+}
